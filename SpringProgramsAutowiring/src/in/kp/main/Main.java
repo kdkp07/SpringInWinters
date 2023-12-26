f9f54bd6c6a52a836eb2f49cb08543b50ce4aff0
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2023-12-26 17:01:27 +0530

    DI using Autowired keyword

---
+++ SpringProgramsAutowiring/src/in/kp/main/Main.java
@@ -0,0 +1,15 @@
+package in.kp.main;
+
+import in.kp.beans.Student;
+import in.kp.resources.SpringConfigFile;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
+        Student std = context.getBean(Student.class);
+        std.display();
+
+    }
+}
---
+++ SpringProgramsAutowiring/src/in/kp/resources/SpringConfigFile.java
@@ -0,0 +1,29 @@
+package in.kp.resources;
+
+import in.kp.beans.Address;
+import in.kp.beans.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfigFile {
+    @Bean
+    public Address createAddr(){
+        Address addr = new Address();
+        addr.setPincode(123442);
+        addr.setCity("Ujjain");
+        addr.setHouseno(234);
+        return addr;
+    }
+
+    @Bean
+    public Student createStd(){
+        Student std = new Student();
+        std.setName("Kuldeep");
+        std.setRollno(232);
+        // here the address field has been set by Autowired keyword inside the Address class
+        return std;
+    }
+
+
+}
