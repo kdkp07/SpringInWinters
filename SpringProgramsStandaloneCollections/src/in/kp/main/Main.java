44cc289c2b525188401b50f7be8d674c152bff4f
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2023-12-29 23:22:45 +0530

    Learnt about spring standalone

---
+++ SpringProgramsStandaloneCollections/src/in/kp/main/Main.java
@@ -0,0 +1,13 @@
+package in.kp.main;
+
+import in.kp.beans.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/in/kp/resources/applicationContext.xml");
+        Person person = context.getBean("person1",Person.class);
+        System.out.println(person);
+    }
+}
